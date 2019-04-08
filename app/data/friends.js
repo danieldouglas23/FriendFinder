233d03bd3ec9bac9dbe94a1b1c7e3e28705ed7e4
Daniel D <danieldouglas23@gmail.com>
2019-04-08 10:04:57 -0600

    added updated files

---
+++ app/data/friends.js
@@ -0,0 +1,89 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the potential friends
+// ===============================================================================
+
+var friendsArray = [
+    {
+        "name":"Anna Kendrick",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,1,4,4,5,1,2,5,4,1
+          ]
+      },
+      {
+        "name":"Tessa Watts",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,5,2,2,1,5,4,1,2,5
+          ]
+      },
+      {
+        "name":"Jennifer Aniston",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+          3,4,2,1,5,4,2,4,5,3
+          ]
+      },
+      {
+        "name":"Kristen Bell",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+           5,1,2,3,4,5,1,2,3,4
+          ]
+      },
+      {
+        "name":"Halle Berry",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,5,4,2,4,3,5,3,4,1
+          ]
+      },
+      {
+        "name":"Gabrielle Union",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            2,4,3,2,4,3,1,5,1,5
+          ]
+      },
+      {
+        "name":"Reese Witherspoon",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,4,3,4,2,4,1,5,5,5
+          ]
+      },
+      {
+        "name":"Jennifer Connelly",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,2,4,3,2,4,1,5,5,1
+          ]
+      },
+      {
+        "name":"Jada Pinkett",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            3,3,1,1,1,1,5,5,2,5
+          ]
+      },
+      {
+        "name":"Charlize Theron",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,2,4,2,5,5,1,1,3,3
+          ]
+      }
+                              
+      
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
+
+
+
+
+
+
+
