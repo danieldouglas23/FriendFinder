a171cbeac263938cc618ffb7b225de43bd198423
Daniel D <danieldouglas23@gmail.com>
2019-04-10 16:27:47 -0600

    added updated files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,43 +11,67 @@ var friendsData = require("../data/friends.js");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+module.exports = function (app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
-  });
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
+    // var scoresArray = [];
 
-  app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-   friendsData.push(req.body);
+    app.post("/api/friends", function (req, res) {
+        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+        // It will do this by sending out the value "true" have a table
+        // req.body is available since we're using the body parsing middleware
+        console.log(req.body.scores);
+        // friendsData.push(req.body);
+        // var userScore = req.body.scores;
+        var bestMatch = {
+            name: "",
+            photo: "",
+            diff: 100
+        };
 
-    // compatibility logic
-    
-  });
+        var difference = 0;
+        // compatibility logic
+        for (var i = 0; i < friendsData.length; i++) {
+            var currentFriend = friendsData[i];
+            difference = 0;
+            for (var j = 0; j < req.body.scores.length; j++) {
+                difference += Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(req.body.scores[j]));
+            }
+            if (difference < bestMatch.diff) {
+                bestMatch.name = currentFriend.name;
+                bestMatch.photo = currentFriend.photo;
+                bestMatch.diff = difference;
+            }
+        }
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+        friendsData.push(req.body);
+        res.json(bestMatch);
 
-//   app.post("/api/clear", function(req, res) {
-//     // Empty out the arrays of data
-//     friendsData.length = 0;
+    });
 
-//     res.json({ ok: true });
-//   });
+    // ---------------------------------------------------------------------------
+    // I added this below code so you could clear out the table while working with the functionality.
+    // Don"t worry about it!
+
+    //   app.post("/api/clear", function(req, res) {
+    //     // Empty out the arrays of data
+    //     friendsData.length = 0;
+
+    //     res.json({ ok: true });
+    //   });
 };
